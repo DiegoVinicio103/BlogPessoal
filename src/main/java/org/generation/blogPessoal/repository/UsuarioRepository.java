1de8e1183a327cbdf5dcfe10b1f85641e1327f20
DiegoVinicio103 <diego_vinicio103@hotmail.com>
2021-02-03 10:41:47 -0300

    Finalização e ajustes - camadas segurança

--- src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
+++ src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -8,5 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	public Optional<Usuario> findByUsuario(String usuario);
 	
-
 }
